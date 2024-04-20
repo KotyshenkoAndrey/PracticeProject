6c2924b4c94c1c82e7406d91e788b7953755e505
kotyshenko25 <kotyshenko25@yandex.ru>
2024-04-20 17:46:42 +0300

    Add web client

---
+++ Systems/Web/PracticeProject.Web/Pages/Auth/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using PracticeProject.Web.Pages.Auth.Models;
+
+namespace PracticeProject.Web.Pages.Auth.Services;
+
+public interface IAuthService
+{
+    Task<LoginResult> Login(LoginModel loginModel);
+    Task Logout();
+}
---
+++ Systems/Web/PracticeProject.Web/Program.cs
@@ -0,0 +1,26 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using PracticeProject.Web;
+using PracticeProject.Web.Pages.Auth.Services;
+using PracticeProject.Web.Providers;
+using PracticeProject.Web.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Settings.ApiRoot) });
+
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+await builder.Build().RunAsync();
